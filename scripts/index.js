6fe1807d569e54e1246bb981cf4f743576ccf639
Adri <a.fernandezrodriguez2003@gmail.com>
2023-11-06 21:55:13 +0100

    Empezamos con los estilos

--- scripts/index.js
+++ scripts/index.js
@@ -13,18 +13,14 @@ document.addEventListener("click", () => {
     });
 });
 
-// Handle the search button click
 const btnSearch = document.getElementById("btnSearch");
 btnSearch.addEventListener("click", () => {
     const searchText = document.getElementById("searchProduct").value.trim().toLowerCase();
 
-    // Filter products based on the search text
     const filteredProducts = inventory.filter((product) => product.name.toLowerCase().includes(searchText));
 
-    // Clear the table
     const cleanTable = document.getElementById("cleanTable");
     cleanTable.innerHTML = "";
 
-    // List the filtered products
     listProducts(filteredProducts);
 });
